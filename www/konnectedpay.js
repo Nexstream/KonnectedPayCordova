b67e796a818f16bb7f41a3fd4371b6b672f73b1e
TJ Woon <tj@cloudsky.org>
2016-01-04 16:22:05 +0800

    Fixed JS typo...

--- www/konnectedpay.js
+++ www/konnectedpay.js
@@ -118,7 +118,7 @@ KonnectedPay.prototype = {
 
                         break;
                 }
-            });
+            };
             req.open("GET", url);
             req.send();
         };
